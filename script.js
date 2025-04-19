81bd70feb39d1a8570bf461eed45a1636fe9b430
ArushKhasru <ak3949235@gmail.com>
2025-04-19 07:59:29 +0530

    script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function secondsToMinutesSeconds(seconds) {
 }
 
 async function getSongs() {
-    let a = await fetch("/songs/")
+    let a = await fetch("http://127.0.0.1:3000/songs/")
     let response = await a.text();
     console.log(response)
     let div = document.createElement("div")
